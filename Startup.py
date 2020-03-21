27076fe8e698a5cf08c44526213d60392ba734e9
Ascanius <adrian-beckmann@t-online.de>
2020-03-21 18:44:42 +0100

    added prestartup

--- Startup.py
+++ Startup.py
@@ -3,21 +3,30 @@
 #Loads all used files and starts the UI
 
 #import used libraries
-import functions.py
+import functions.py as fkt
 import tkinter
+import os
 
 #Startup sequence
-#Load Language File
-lang = 0
-file = open ("config.txt","r")
-lang = file.readline(3)
+#Check for config file
 
-IF lang = 0 THEN
+if not os.path.exists("config.txt") then:
+    fkt.CreateConfigFile()
+    fkt.AddToConfigFile(0,"NULL","NO","NO","database.db")
+
+#Load Language File
+prestartup = tk()
+prestartup.title("")
+file = open ("config.txt","r")
+lang = file.readline(4)
+IF lang = 0 THEN:
     language = open ("DEU.txt","r")
-ELIF lang = 1 THEN
+ELIF lang = 1 THEN:
     language = open ("ENU.txt","r")
-ELSE
+ELSE:
     messagebox.showerror("Error","CODE SP 0001")
+prestartup.mainloop()
+
 
 
 startup = tk()
